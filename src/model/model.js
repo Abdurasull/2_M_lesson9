ba1ed105f70c2125e83b733f2cb7f93d5dd05948
Abdurasull <abdurasulsalimov94@gmail.com>
2025-04-29 20:57:27 +0500

    ---

--- src/model/model.js
+++ src/model/model.js
@@ -1,14 +1,14 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import { serverConfig } from '../config.js';
 const {dbPath} = serverConfig
 
 export const model = (req, res, next) => {
     req.writeFile = async function(fileName, data){
-        await fs.writeFileSync(dbPath(fileName), JSON.stringify(data, null, 4));
+        await fs.writeFile(dbPath(fileName), JSON.stringify(data, null, 4));
         return true;
     };
     req.readFile = async function(fileName){
-        let data = await fs.readFileSync(dbPath(fileName), "utf-8");
+        let data = await fs.readFile(dbPath(fileName), "utf-8");
         return data ? JSON.parse(data) : [];
     };
     return next();    
